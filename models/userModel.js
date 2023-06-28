84657e8512e6fa7bb08d192224c19c04146ff2c1
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-06-28 19:37:25 +0530

    contacts-backend

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    username : {
+        type: String,
+        required: [true, "Please add the username"]
+    },
+    email : {
+        type: String,
+        required: [true, "Please add the user email"],
+        unique: [true, "Email Address Already Registered"]
+    },
+    password : {
+        type: String,
+        required: [true, "Please enter password"]
+    }
+},{
+    timestamps : true
+});
+
+module.exports = mongoose.model("User", userSchema);
